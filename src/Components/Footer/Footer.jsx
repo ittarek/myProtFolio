0f45c98f960a5e3d5378586d493390e845019bbc
Md Tariqul Islam <ittarek551@gmail.com>
2025-11-18 12:43:07 +0600

    add footer link

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -14,27 +14,27 @@ export const Footer = () => {
         <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
           <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
             <div className="px-5 py-2">
-              <Link to="/home" className="text-base text-gray-500 hover:text-gray-900">
+              <Link to="/" className="text-base text-gray-500 hover:text-gray-400">
                 Home
               </Link>
             </div>
             <div className="px-5 py-2">
-              <Link to="/about" className="text-base text-gray-500 hover:text-gray-900">
+              <Link to="/about" className="text-base text-gray-500 hover:text-gray-400">
                 About
               </Link>
             </div>
 
             <div className="px-5 py-2">
               <Link
-                to="/portfolio"
-                className="text-base text-gray-500 hover:text-gray-900">
+                to="/myProtFolio"
+                className="text-base text-gray-500 hover:text-gray-400">
                 Portfolio
               </Link>
             </div>
-            <div className="px-5 py-2">
+            <div className="px-5 py-2 cursor-pointer">
               <div
                 onClick={handleDownload}
-                className="text-base text-gray-500 hover:text-gray-900">
+                className="text-base text-gray-500 hover:text-gray-400">
                 Resume
               </div>
             </div>
@@ -43,7 +43,7 @@ export const Footer = () => {
             <a
               target="_blank"
               href="https://www.facebook.com/md.tariqul.islam.428023/"
-              className="text-gray-400 hover:text-gray-500">
+              className="text-gray-400 hover:text-gray-400">
               <span className="sr-only">Facebook</span>
               <FaceBookIcon />
             </a>
@@ -51,7 +51,7 @@ export const Footer = () => {
             <a
               target="_blank"
               href="https://github.com/ittarek"
-              className="text-gray-400 hover:text-gray-500">
+              className="text-gray-400 hover:text-gray-400">
               <span className="sr-only">GitHub</span>
               <GithubIcon />
             </a>
