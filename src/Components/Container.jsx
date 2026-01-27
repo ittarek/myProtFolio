0c19afdc494357e944f050af28e1af6628e38339
Md Tariqul Islam <ittarek551@gmail.com>
2026-01-27 22:13:54 +0600

    add max w

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = ({children}) => {
     return (
-      <div className="max-w-[2520px] mx-auto xl:px-5 md:px-8 sm:px-2 px-4">
+      <div className="max-w-[1500px] mx-auto xl:px-5 md:px-8 sm:px-2 px-4">
         {children}
       </div>
     );
