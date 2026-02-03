1e84860a90c6f35110c8f191c02df2ca551bce97
Md Tariqul Islam <ittarek551@gmail.com>
2026-02-03 12:59:49 +0600

    add scroll to top

--- src/Components/Shared/ScrollToTop.jsx
+++ src/Components/Shared/ScrollToTop.jsx
@@ -1,11 +1,21 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 export const ScrollToTop = () => {
-  const pathname = useLocation();
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    // Disable ScrollTrigger temporarily
+    ScrollTrigger.getAll().forEach(st => st.disable());
+
+    // Scroll to top
+    window.scrollTo(0, 0);
+
+    // Re-enable after a delay
+    setTimeout(() => {
+      ScrollTrigger.getAll().forEach(st => st.enable());
+      ScrollTrigger.refresh();
+    }, 200);
   }, [pathname]);
 
   return null;
--- src/LayOut/MainLayOut.jsx
+++ src/LayOut/MainLayOut.jsx
@@ -1,10 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import NavBar from '../NavigationBar/NavBar';
 import { Footer } from '../Components/Footer/Footer';
+import { ScrollToTop } from '../Components/Shared/ScrollToTop';
+import { useEffect } from 'react';
 
 const MainLayOut = () => {
+
   return (
     <>
+      <ScrollToTop />
       <NavBar />
       <div className="min-h-screen">
         <Outlet />
