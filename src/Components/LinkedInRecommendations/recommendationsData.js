bccbe6ebf3b258a8cbaf71f08624bba17a7c243c
Md Tariqul Islam <ittarek551@gmail.com>
2026-02-13 20:42:49 +0600

    add memo

---
+++ src/Components/LinkedInRecommendations/recommendationsData.js
@@ -0,0 +1,58 @@
+import traventCEO from '../../assets/ClientImage/ceo at travent.png';
+import assadalauddin from '../../assets/ClientImage/assadAlauddin.png';
+import jubaerImage from '../../assets/ClientImage/jubaer Ibn Zahir.png';
+import MehediHasan from '../../assets/ClientImage/mehedi.png';
+
+
+export const recommendationsData = [
+  {
+    id: 1,
+    name: 'Faisal Alhosani',
+    position: 'CEO',
+    company: 'Travent',
+    image: traventCEO,
+    linkedin: 'https://www.linkedin.com/in/faisal-alhosani/',
+    date: 'October 2025',
+    recommendation:
+      'I had the pleasure of working with Md. Tariqul Islam during his time as a Front-End Software Developer at Travent. Over the six months he spent with us, he proved to be a dedicated and talented developer who consistently delivered high-quality work.At Travent, Tariqul made a noticeable impact by improving the performance and responsiveness of our web platform. He designed clean, user-friendly interfaces using React.js and introduced reusable components and custom hooks that significantly enhanced the scalability and maintainability of our code. His ability to apply the right data structures and algorithms helped optimize key parts of the application.What I appreciated most was his collaborative attitude and eagerness to grow. He worked well with both the tech and product teams, welcomed feedback with a positive mindset, and was always ready to help others. His problem-solving skills and attention to detail were a real asset to our team.I fully recommend Tariqul for any front-end development opportunity. He would be a strong addition to any team looking for someone reliable, skilled, and team-oriented.',
+    relationship:
+      "Faisal Alhosani was Tariqul's CEO at Travent and worked closely with him on multiple projects",
+  },
+  {
+    id: 2,
+    name: 'Assadaldin Osman',
+    position: 'CEO',
+    company: 'Nilvion',
+    image: assadalauddin,
+    date: 'October 2025',
+    linkedin: 'https://www.linkedin.com/in/assadaldin/',
+    recommendation:
+      'I had the pleasure of working with Md. Tariqul Islam as a Front-End Software Developer for around 6 months in our company. During this time, he consistently demonstrated professionalism, creativity, and a strong command of front-end technologies.Tariqul played a key role in improving the performance of our website, designing user-friendly UIs, and building responsive components using React.js. He also developed several reusable and custom hooks, which significantly enhanced the efficiency and maintainability of our codebase.Moreover, he applied data structure and algorithm knowledge to optimize rendering and data processing. His approach to data fetching was efficient and scalable, and he implemented modern error-handling techniques and followed industry-standard best practices in his coding style.His dedication, problem-solving ability, and teamwork were truly impressive. I highly recommend him for any front-end development role. He would be a valuable asset to any team.',
+    relationship: 'Assadaldin Osman worked directly with Tariqul',
+  },
+  {
+    id: 3,
+    name: 'Jubaer Ibn Zahir',
+    position: ' Project Manager',
+    company: 'Programming hero',
+    image: jubaerImage,
+    linkedin: 'https://www.linkedin.com/in/jubaer-ibn-zahir-9471841aa/',
+    date: 'November 2024',
+    recommendation:
+      'I have mentored Md Tariqul Islam in my boot camp for the past few months. He is a very productive and multi-skilled person with vast knowledge. He is a careful, active, self-motivated, deadline-oriented, passionate, intelligent team player, and has a great vision for his work. His focus keeps everything moving smoothly; he makes sure all the deadlines are met and that whatever project he is working on meets the highest standards. Thanks to his interpersonal skills, he has excellent relations with team members and the mentor(me). Highly recommended.',
+    relationship:
+      'Jubaer Ibn Zahir Was Mentor of Tariqul Islam in Programming Hero Bootcamp',
+  },
+  {
+    id: 4,
+    name: 'Mehedi Hasan',
+    position: 'Software Developer',
+    company: 'Horizon Plus',
+    image: MehediHasan,
+    date: 'December 2024',
+    linkedin: 'https://www.linkedin.com/in/mehedi1803hasan/',
+    recommendation:
+      'Md Tariqul is a talented MERN stack frontend developer known for his hard work and eagerness to learn new technologies. As a supportive and reliable team player, I wish him continued success in all his future endeavors.',
+    relationship: 'Mehedi Hasan Work on same BootCamp with Tariqul',
+  },
+];
